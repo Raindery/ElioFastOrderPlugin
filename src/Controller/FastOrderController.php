835925f7c253358351b28968d27a7b991226dee1
Ivan Mezhetsky <vfeodorovm@yandex.com>
2021-12-26 02:58:14 +0300

    Add temp general mechanic for add product to shopping cart

--- src/Controller/FastOrderController.php
+++ src/Controller/FastOrderController.php
@@ -2,14 +2,19 @@
 
 namespace Elio\FastOrder\Controller;
 
+use Shopware\Core\Checkout\Cart\Cart;
+use Shopware\Core\Checkout\Cart\LineItem\LineItem;
+use Shopware\Core\Checkout\Cart\LineItemFactoryRegistry;
+use Shopware\Core\Checkout\Cart\SalesChannel\CartService;
 use Shopware\Core\Content\Product\Exception\ProductNotFoundException;
-use Shopware\Core\Content\Product\ProductEntity;
+use Shopware\Core\Content\Product\ProductCollection;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\ContainsFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\PrefixFilter;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Controller\StorefrontController;
 
 use Symfony\Component\HttpFoundation\Response;
@@ -20,12 +25,18 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class FastOrderController extends StorefrontController
 {
+    private LineItemFactoryRegistry $factory;
+
     private EntityRepositoryInterface $productsRepository;
 
+    private CartService $cartService;
 
-    public function __construct(EntityRepositoryInterface $productsRepository)
+
+    public function __construct(EntityRepositoryInterface $productsRepository, CartService $cartService, LineItemFactoryRegistry $factory)
     {
         $this->productsRepository = $productsRepository;
+        $this->cartService = $cartService;
+        $this->factory = $factory;
     }
 
     /**
@@ -35,21 +46,48 @@ class FastOrderController extends StorefrontController
      */
     public function showFastOrderForm(Context $context) : Response
     {
-        $productNumber = '92bdff6b279945b4aaaea7995b4421e8';
-        return $this->renderStorefront('@ElioFastOrder/storefront/form/fast-order.html.twig', ['product' => $this->getProductByProductNumber($context, $productNumber)]);
+        $productNumber = 'f7';
+        return $this->renderStorefront('@ElioFastOrder/storefront/form/fast-order.html.twig', ['products' => $this->getProductByProductNumber($context, $productNumber)]);
     }
 
-    private function getProductByProductNumber(Context $context, string $productNumber):ProductEntity
+
+    /**
+     * @Route ("/fast-order/add-to-cart", name="store-api.fast-order.add-to-card", methods={"GET"})
+     * @param SalesChannelContext $context
+     * @param Cart $cart
+     * @return Response
+     */
+    public function addProductsToCart(SalesChannelContext $context, Cart $cart) : Response
+    {
+        $productNumber = 'f7';
+        $product = $this->getProductByProductNumber($context->getContext(), $productNumber)->first();
+
+        $lineItem = $this->factory->create([
+            'type' => LineItem::PRODUCT_LINE_ITEM_TYPE,
+            'referencedId' => $product->getId(),
+            'quantity'=> 10
+        ], $context);
+
+        $this->cartService->add($cart, $lineItem, $context);
+
+        return $this->redirectToRoute('frontend.checkout.cart.page');
+    }
+
+    /**
+     * @param Context $context
+     * @param string $productNumber
+     * @return ProductCollection
+     */
+    private function getProductByProductNumber(Context $context, string $productNumber): ProductCollection
     {
         $criteria = new Criteria();
         $criteria->addFilter(new EqualsFilter('available', '1'));
-        $criteria->addFilter(new ContainsFilter('productNumber', $productNumber));
+        $criteria->addFilter(new PrefixFilter('productNumber', $productNumber));
         $criteria->setLimit(10);
 
+        $product = $this->productsRepository->search($criteria, $context)->getEntities();
 
-        $product = $this->productsRepository->search($criteria, $context)->getEntities()->first();
-
-        if($product === null){
+        if($product->count() === 0){
             throw new ProductNotFoundException('');
         }
 
