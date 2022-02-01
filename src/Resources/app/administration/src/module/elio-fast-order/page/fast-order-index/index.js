003fec23e1919971bea21180778a8f6721a95c7c
Ivan Mezhetsky <vfeodorovm@yandex.com>
2022-02-01 17:22:10 +0300

    Added fetch fastOrderEntities and fast-order-index template in fast-order-index component

--- src/Resources/app/administration/src/module/elio-fast-order/page/fast-order-index/index.js
+++ src/Resources/app/administration/src/module/elio-fast-order/page/fast-order-index/index.js
@@ -1,9 +1,35 @@
+import template from './fast-order-index.html.twig';
+
+const {Criteria} = Shopware.Data;
+
 Shopware.Component.register('fast-order-index', {
-    template: '<h1> Hello, world!</h1>',
+    inject: ['repositoryFactory'],
+    template,
+
+    data: function () {
+        return {
+            fastOrderEntities: undefined,
+        }
+    },
 
     metaInfo() {
         return {
           title: this.$createTitle()
         };
+    },
+
+    computed: {
+        fastOrderRepository(){
+            return this.repositoryFactory.create('fast_order');
+        }
+    },
+
+    created() {
+        this.fastOrderRepository
+            .search(new Criteria(), Shopware.Context.api)
+            .then((result) => {
+                this.fastOrderEntities = result;
+                console.log(this.fastOrderEntities);
+            })
     }
 });
